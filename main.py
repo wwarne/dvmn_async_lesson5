4045cd8f60b9a9b8448648148a6cab14d7ae8404
Dmitry Plevkov <wwarne@gmail.com>
2020-05-05 12:56:55 +0300

    Rename nursery to tg (task group)

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import asyncio
 import logging.config
 from tkinter import messagebox
 
+from anyio import create_task_group
+
 import gui
 from exceptions import MinechatException
 from gui import TkAppClosed
 from history_client import history_restore, history_save
-from anyio import create_task_group
 from settings import read_settings, get_logging_settings
 from watchdog import handle_connection
 
@@ -18,43 +19,43 @@ async def run_chat_internals(
         writer_port: int,
         access_token: str,
         history_path: str,
-):
+) -> None:
     """Runs all coroutines."""
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
     history_queue = asyncio.Queue()
     watchdog_queue = asyncio.Queue()
-    async with create_task_group() as nursery:
-        await nursery.spawn(gui.draw,
-                            messages_queue,
-                            sending_queue,
-                            status_updates_queue,
-                            )
+    async with create_task_group() as tg:
+        await tg.spawn(gui.draw,
+                       messages_queue,
+                       sending_queue,
+                       status_updates_queue,
+                       )
         await history_restore(
             path=history_path,
             messages_queue=messages_queue,
         )
-        await nursery.spawn(history_save,
-                            history_path,
-                            history_queue,
-                            )
-        await nursery.spawn(handle_connection,
-                            reader_host,
-                            reader_port,
-                            writer_host,
-                            writer_port,
-                            access_token,
-                            messages_queue,
-                            sending_queue,
-                            history_queue,
-                            status_updates_queue,
-                            watchdog_queue,
-                            )
+        await tg.spawn(history_save,
+                       history_path,
+                       history_queue,
+                       )
+        await tg.spawn(handle_connection,
+                       reader_host,
+                       reader_port,
+                       writer_host,
+                       writer_port,
+                       access_token,
+                       messages_queue,
+                       sending_queue,
+                       history_queue,
+                       status_updates_queue,
+                       watchdog_queue,
+                       )
 
 
 def run_chat() -> None:
-    """Entry point to setup and start the application."""
+    """Entry point to initialize and start the application."""
     total_settings = read_settings()
     logger_dict_config = get_logging_settings(total_settings['loglevel'])
     logging.config.dictConfig(logger_dict_config)
